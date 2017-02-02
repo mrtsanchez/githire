2914faf677637aa3d1ca049a3c86ed2b45353e20
marta sanchez <sanchez.marta@gmail.com>
2017-02-02 10:44:56 -0800

    display the results int he site

--- js/githire-interface.js
+++ js/githire-interface.js
@@ -1,9 +1,21 @@
 var GitHubSearch = require('./../js/githire.js').gitHubModule;
 
-// function filterCandidates(){
-//
-//
-// }
+function displayResults(candidatesInformation){
+
+  $("#spinner").hide();
+
+  for (var i = 0; i < candidatesInformation.length; i++) {
+    $("#results").append('<div class="col-md-4">'+
+                        '<div class="panel panel-default">'+
+                        '<div class="panel-heading">'+
+                        '<h3 class="panel-title title">'+ candidatesInformation[i].name +'"</h3>"'
+
+
+
+                      );
+  }
+}
+
 
 $(document).ready(function() {
 
@@ -16,6 +28,9 @@ $(document).ready(function() {
 
     event.preventDefault();
 
+    $("#spinner").show()
+    $(".index-content").hide();
+
     // $("#error").hide();
     // $(".restart").empty();
 
@@ -23,9 +38,14 @@ $(document).ready(function() {
     input_language = $("#language").val();
     input_experience = $("#experience").val();
 
+    console.log(input_location);
+    console.log(input_language);
+    console.log(input_experience);
+
+
     currentGitHubSearch = new GitHubSearch();
 
-    currentGitHubSearch.devsLookup(input_location, input_language, input_experience);
+    currentGitHubSearch.devsLookup(input_location, input_language, input_experience, displayResults);
     // currentGitHubSearch.reposLookup(input_username, displayRepos);
 
   });
--- js/githire.js
+++ js/githire.js
@@ -18,7 +18,9 @@ function getRepos(username){
   return $.get('https://api.github.com/users/' + username + '/repos?access_token='+ apiKey + '&sort=push&type=owner&per_page=100');
 }
 
-GitHubSearch.prototype.devsLookup = function(location, language, experience){
+GitHubSearch.prototype.devsLookup = function(location, language, experience, displayResults){
+
+  var candidatesInformation = [];
 
   (getDevelopers(location,language, experience, 1)).then(function(response){
 
@@ -28,8 +30,14 @@ GitHubSearch.prototype.devsLookup = function(location, language, experience){
 
     var devs = [];
     var pages = Math.ceil(totalDevs/100);
-    for (var l= 1; l <= pages; l++) {
-      devs.push(getDevelopers(location,language, experience, l));
+    if (pages > 10) {
+      for (var l= 1; l <= 10; l++) {
+        devs.push(getDevelopers(location,language, experience, l));
+      }
+    } else {
+      for (var l= 1; l <= pages; l++) {
+        devs.push(getDevelopers(location,language, experience, l));
+      }
     }
 
     $.when.apply($, devs).then(function(){
@@ -51,67 +59,70 @@ GitHubSearch.prototype.devsLookup = function(location, language, experience){
           allrepos.push(arguments[m][0]);
         }
         console.log(allrepos);
+
+        var candidates = [];
+
+        // Filter the candidates by their repos
+        for(var n = 0; n < allrepos.length; n++){
+          var topRepos = [];
+          if (experience === "entry") {
+            for (var o = 0; o < allrepos[n].length; o++){
+              if (allrepos[n][o].language === language && allrepos[n][o].watchers >= 1){
+                topRepos.push(allrepos[n][o]);
+              }
+            }
+            if(allrepos[n].length >= 40 && topRepos.length >= 2){
+              candidates.push(topRepos[0].owner.login);
+            }
+            } else if (experience === "junior") {
+              for (var o = 0; o < allrepos[n].length; o++){
+                if (allrepos[n][o].language === language && allrepos[n][o].forks_count >= 2 && allrepos[n][o].watchers >= 2){
+                  topRepos.push(allrepos[n][o]);
+                }
+              }
+              if(topRepos.length >= 5){
+                candidates.push(topRepos[0].owner.login);
+              }
+            } else {
+              for (var o = 0; o < allrepos[n].length; o++){
+                if (allrepos[n][o].language === language && allrepos[n][o].forks_count >= 1 && allrepos[n][o].watchers >= 10 && allrepos[n][o].stargazers_count >= 2){
+                  topRepos.push(allrepos[n][o]);
+                }
+              }
+              if(topRepos.length >= 2){
+                candidates.push(topRepos[0].owner.login);
+            }
+          }
+        }
+
+        console.log(candidates);
+
+
+        // Go to github again and retrieve the info for the candidates that made the cut
+        var candidatesData = [];
+
+        for (var p = 0; p < candidates.length; p++){
+          candidatesData.push($.get('https://api.github.com/users/' + candidates[p] +'?access_token=' + apiKey));
+        }
+
+        $.when.apply($, candidatesData).done(function(){
+
+          for(var k = 0, len = arguments.length; k < len; k++){
+            candidatesInformation.push(arguments[k][0]);
+          }
+
+          console.log(candidatesInformation);
+          displayResults(candidatesInformation);
+
+        }); // End of the third when
+
       }); // End of the second when
 
     }); // End of the first when
-
-
-
-    // $.when(getDevelopers(location, language, response)).done(function(usernames){
-    //   console.dir(usernames);
-    //   console.log(usernames.length);
-    //
-    // }); // end of the first when
+  }).fail(function(error){
+  // displayErrorMessage(username);
   }); // end the initial Github API call
 
+}
 
-
-    // extract the actual repos from the array of objects created in the last step
-    // $.when.apply($, repos).done(function(){
-    //   var allrepos = [];
-    //   for(var m = 0, len = arguments.length; m < len; m++){
-    //     allrepos.push(arguments[m][0]);
-    //   }
-    //   console.log(allrepos);
-    //
-    //   var candidates = [];
-    //
-    //   // Filter the candidates by their repos
-    //   for(var n = 0; n < allrepos.length; n++){
-    //     var topRepos = [];
-    //     for (var o = 0; o < allrepos[n].length; o++){
-    //       if (allrepos[n][o].language === language && allrepos[n][o].forks_count >= 2 && allrepos[n][o].watchers >= 2 && allrepos[n][o].stargazers_count >= 2){
-    //         topRepos.push(allrepos[n][o]);
-    //       }
-    //     }
-    //     if(topRepos.length >= 2){
-    //       candidates.push(topRepos[0].owner.login);
-    //     }
-    //   }
-    //   // Go to github again and retrieve the info for the candidates that made the cut
-    //   var candidatesInfo = [];
-    //   for (var p = 0; p < candidates.length; p++){
-    //     candidatesInfo.push($.get('https://api.github.com/users/' + candidates[p] +'?access_token=' + apiKey));
-    //   }
-    //   console.log(candidates);
-    //
-    //   $.when.apply($, candidatesInfo).done(function(){
-    //
-    //     for(var q = 0, len = arguments.length; q < len; q++){
-    //       candidatesInformation.push(arguments[q][0]);
-    //     }
-    //     console.log(candidatesInformation);
-    //   }); //end the second when
-    //
-    // }); // End the first when
-
-
-
-  // return candidatesInformation;
-};
-
-  // GitHubSearch.prototype.reposLookup = function (){
-  //
-  // };
-
-  exports.gitHubModule = GitHubSearch;
+exports.gitHubModule = GitHubSearch;
